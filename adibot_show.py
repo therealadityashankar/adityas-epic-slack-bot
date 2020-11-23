7e1e2b46aef03d054b1af68ffa6c7bcb61df11e3
therealadityashankar <me@adityashankar.xyz>
2020-11-23 12:37:45 +0530

    modified and improved code; added *.swp to gitignore

---
+++ adibot_show.py
@@ -0,0 +1,89 @@
+from config import config
+import requests
+
+
+def adibot_show_service(message, say, words):
+    begin = "adi-bot show"
+    text = message["text"].strip()
+
+    # removes begin string and an additional space after that
+    resttext = text[len(begin) + 1 :]
+
+    if len(words) == 2:
+        github_url = "https://github.com/therealadityashankar/adityas-epic-slack-bot"
+        say("to see a list of options of what adi-bot can" + "show, see" + github_url)
+
+    elif len(words) == 3:
+        if words[-1] == "cat":
+            url = get_cat_images(1)[0]["url"]
+            alt = "cat image"
+            ftext = "here is a cat pic"
+
+        elif words[-1] == "dog":
+            url = get_dog_images(1)[0]["url"]
+            alt = "dog image"
+            ftext = "here is a dog pic"
+
+        else:
+            return
+
+        blocks = [
+            create_img_block(url, alt),
+            create_markdown_block(ftext),
+        ]
+        say(blocks=blocks)
+
+    # basically it ends in cats and it has one word in between
+    elif len(words) == 4:
+        try:
+            num = int(words[-2])
+        except ValueError:
+            say(f"{num} could not be converted into an integer")
+            return
+
+        if num > 3 or num < 1:
+            e = f"\
+the number (what you've said as {num}) should be between 1 and 3"
+            say(e)
+            return
+
+        if words[-1] == "cats":
+            images = get_cat_images(num)
+            alt = "cat image"
+            text = f"here are {num} cat pics"
+
+        elif words[-1] == "dogs":
+            images = get_dog_images(num)
+            alt = "dog image"
+            text = f"here are {num} dog pics"
+
+        else:
+            return
+
+        blocks = [create_img_block(pic["url"], alt) for pic in images]
+        blocks.append(create_markdown_block(text))
+        say(blocks=blocks)
+
+
+def create_img_block(url, alt):
+    return {"type": "image", "image_url": url, "alt_text": alt}
+
+
+def create_markdown_block(text):
+    return {"type": "section", "text": {"type": "mrkdwn", "text": text}}
+
+
+def get_dog_images(count):
+    resp = requests.get(
+        f"https://api.thedogapi.com/v1/images/search?limit={count}",
+        headers={"x-api-key": config["DOG_API_KEY"]},
+    )
+    return resp.json()
+
+
+def get_cat_images(num):
+    resp = requests.get(
+        f"https://api.thecatapi.com/v1/images/search?limit={num}",
+        headers={"x-api-key": config["CAT_API_KEY"]},
+    )
+    return resp.json()
--- app.py
+++ app.py
@@ -3,109 +3,36 @@
 import os
 import yaml
 import requests
+
 # Use the package we installed
 from slack_bolt import App
+from config import config
+from adibot_show import adibot_show_service
 
 # Initializes your app with your bot token and signing secret
-with open("config.yaml") as f:
-    config = yaml.safe_load(f)
-    app = App(
-        token=config['token'],
-        signing_secret=config['signing_secret']
-    )
+app = App(token=config["token"], signing_secret=config["signing_secret"])
+
 
 @app.message("adi-bot")
 def adibotservice(message, say):
-    text = message['text'].strip()
+    text = message["text"].strip()
+    words = [word.strip() for word in text.split(" ") if word.strip() != ""]
+
+    if not text.startswith("adi-bot"):
+        return
 
     if text == "adi-bot":
-        say("you can see all of adi-bot's features at its docs at https://github.com/therealadityashankar/adityas-epic-slack-bot")
+        say(
+            "you can see all of adi-bot's features at its docs at \
+https://github.com/therealadityashankar/adityas-epic-slack-bot"
+        )
         return
 
     elif text.startswith("adi-bot show"):
-        return adibot_show_service(message, say)
+        return adibot_show_service(message, say, words)
 
-def adibot_show_service(message, say):
-    begin = "adi-bot show"
-    text = message['text'].strip()
-
-    # removes begin string and an additional space after that
-    resttext = text[len(begin) + 1:]
-
-    if not text.startswith(begin): return
-
-    if text == begin:
-        say("to see a list of options of what adi-bot can show, see https://github.com/therealadityashankar/adityas-epic-slack-bot")
-
-    elif resttext == "cat":
-        resp = requests.get("https://api.thecatapi.com/v1/images/search", headers={"x-api-key":config['CAT_API_KEY']})
-        cat_image_url = resp.json()[0]['url']
-        img_block = {"type": "image", "image_url":cat_image_url, "alt_text": "cat image"} 
-        text_block = {"type":"section", "text": {"type":"mrkdwn", "text":"here is a cat pic"}}
-        blocks = [img_block,  text_block]
-        say(blocks=blocks)
-
-    # basically it ends in cats and it has one word in between
-    elif resttext.endswith(" cats") and (not " " in resttext[:-len(" cats")]):
-        num = resttext[:-len(" cats")]
-
-        try:
-            num = int(num)
-        except ValueError:
-            say(f"{num} could not be converted into an integer")
-            return
-
-        if num > 3 or num < 1:
-            say(f"the number (what you've said as {num}) should be between 1 and 3")
-            return
-
-        resp = requests.get(f"https://api.thecatapi.com/v1/images/search?limit={num}", headers={"x-api-key":config['CAT_API_KEY']})
-        blocks = []
-
-        for cat_pic in resp.json():
-            cat_image_url = cat_pic['url']
-            img_block = {"type": "image", "image_url":cat_image_url, "alt_text": "cat image"} 
-            blocks.append(img_block)
-
-        text_block = {"type":"section", "text": {"type":"mrkdwn", "text":f"here are {num} cat pics"}}
-        blocks.append(text_block)
-
-        say(blocks=blocks)
-
-    elif resttext == "dog":
-        resp = requests.get("https://api.thedogapi.com/v1/images/search", headers={"x-api-key":config['DOG_API_KEY']})
-        image_url = resp.json()[0]['url']
-        img_block = {"type": "image", "image_url":image_url, "alt_text": "dog image"} 
-        text_block = {"type":"section", "text": {"type":"mrkdwn", "text":"here is a dog pic"}}
-        blocks = [img_block,  text_block]
-        say(blocks=blocks)
-
-    # basically it ends in cats and it has one word in between
-    elif resttext.endswith(" dogs") and (not " " in resttext[:-len(" dogs")]):
-        num = resttext[:-len(" dogs")]
-
-        try:
-            num = int(num)
-        except ValueError:
-            say(f"{num} could not be converted into an integer")
-            return
-
-        if num > 3 or num < 1:
-            say(f"the number (what you've said as {num}) should be between 1 and 3")
-            return
-
-        resp = requests.get(f"https://api.thedogapi.com/v1/images/search?limit={num}", headers={"x-api-key":config['DOG_API_KEY']})
-        blocks = []
-
-        for pic in resp.json():
-            image_url = pic['url']
-            img_block = {"type": "image", "image_url":image_url, "alt_text": "dog image"} 
-            blocks.append(img_block)
-
-        text_block = {"type":"section", "text": {"type":"mrkdwn", "text":f"here are {num} dog pics"}}
-        blocks.append(text_block)
-
-        say(blocks=blocks)
+    else:
+        say(f"I could not understand the command `{text}`")
 
 
 # Start your app
